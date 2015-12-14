d868a7cd297e9dae6915bd55f93bf8d53a1d1f24
AiGreek <deroda.orel@gmail.com>
2015-12-14 16:35:34 +0100

    Update wakeme.py

--- wakeme.py
+++ wakeme.py
@@ -10,6 +10,9 @@ CALENDAR_ID         = 'lotsofnumbersandletters@group.calendar.google.com'
 CLIENT_SECRET_FILE  = 'client_secret_lotsofnumbersandletters.apps.googleusercontent.com.json'
 MP3_FOLDER          = 'mp3'
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 def get_credentials():
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
@@ -36,13 +39,27 @@ def calendar_event_query():
     songfile    = random.choice(os.listdir(MP3_FOLDER))
 
     for i, event in enumerate(events['items']):
+        name = event['summary'].lower()
         try:
             start = event['start']['dateTime'][:-9]
         except KeyError:
             start = ''
-        if start == now:
-            command     =  'mpg123 \'{}/{}\''.format(MP3_FOLDER, songfile) # mpg123 = Raspberry/Linux or afplay = MacOSX
-            os.system(command)
+
+        description = event.get('description', '')
+        repeat = True if description.lower() == 'repeat' else False
+        now = today.strftime('%Y-%m-%dT%H:%M')
+
+        if start >= now:
+            logger.debug('Event #%s, Name: %s, Start: %s', i, name, start)
+
+            if start == now:
+                mp3_files = random.choice(os.listdir(MP3_FOLDER))
+                command = 'afplay \'{}/{}\''.format(MP3_FOLDER, mp3_files) # replace afplay with mpg123 for Raspberry or Linux
+                logger.info('Event %s starting. Playing mp3 file %s...', name, mp3_files)
+                os.system(command)
+
+                if repeat == False:
+                    time.sleep(60)
 
 while True:
     calendar_event_query()
