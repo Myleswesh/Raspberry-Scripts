14e1586f89189d3f316f324594e4b72050f2fc41
AiGreek <deroda.orel@gmail.com>
2016-01-10 20:24:58 +0100

    Update and rename wakeme.py to AlarmClock.py

--- AlarmClock.py
+++ AlarmClock.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 from __future__ import print_function
 import httplib2
 
@@ -7,15 +10,19 @@ from oauth2client import client
 from oauth2client import tools
 
 from datetime import datetime
-import logging, time, random, os
+import logging
+import time
+import random
+import os
+import argparse
 
 try:
-    import argparse
+
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
 
-CALENDAR_ID = 'YOURCALENDAR-ID@group.calendar.google.com'
+CALENDAR_ID = 'YOUR_CALENDAR_ID@group.calendar.google.com'
 CLIENT_SECRET_FILE = 'raspi-wake.json'
 SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
 MP3_FOLDER = 'mp3'
@@ -29,7 +36,7 @@ def get_credentials():
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,'raspberry-wake.json')
+    credential_path = os.path.join(credential_dir, 'raspberry-wake.json')
 
     store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
@@ -38,7 +45,7 @@ def get_credentials():
         flow.user_agent = 'Raspberry Wake'
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
+        else:
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
 
@@ -54,27 +61,20 @@ def calendar_event_query():
     events = service.events().list(singleEvents=True, calendarId=CALENDAR_ID).execute()
 
     for i, event in enumerate(events['items']):
-        name = event['summary'].lower()
         try:
             start = event['start']['dateTime'][:-9]
         except KeyError:
             start = ''
 
         description = event.get('description', '')
-        repeat = True if description.lower() == 'repeat' else False
         now = today.strftime('%Y-%m-%dT%H:%M')
 
-        if start >= now and description == 'wake':
-            logger.debug('Event #%s, Name: %s, Start: %s', i, name, start)
-
+        if start >= now and description.lower() == 'wake':
             if start == now:
                 mp3_files = random.choice(os.listdir(MP3_FOLDER))
                 command = 'mpg123 \'{}/{}\''.format(MP3_FOLDER, mp3_files)
-                logger.info('Event %s starting. Playing mp3 file %s...', name, mp3_files)
                 os.system(command)
-
-                if repeat == False:
-                    time.sleep(60)
+                time.sleep(60)
 
 while True:
     calendar_event_query()
