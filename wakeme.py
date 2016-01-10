339e2910197cd7f0e57b95a2b5222585e33cc8a2
AiGreek <deroda.orel@gmail.com>
2016-01-10 09:23:53 +0100

    Update wakeme.py

--- wakeme.py
+++ wakeme.py
@@ -1,42 +1,57 @@
-from apiclient import discovery
-from oauth2client import client, tools
-import oauth2client
+from __future__ import print_function
 import httplib2
 
-from datetime import datetime
-import time, random, os
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
 
-CALENDAR_ID         = 'lotsofnumbersandletters@group.calendar.google.com'
-CLIENT_SECRET_FILE  = 'client_secret_lotsofnumbersandletters.apps.googleusercontent.com.json'
-MP3_FOLDER          = 'mp3'
+from datetime import datetime
+import logging, time, random, os
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+CALENDAR_ID = 'YOURCALENDAR-ID@group.calendar.google.com'
+CLIENT_SECRET_FILE = 'raspi-wake.json'
+SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
+MP3_FOLDER = 'mp3'
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def get_credentials():
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,'calendar-python-quickstart.json')
+    credential_path = os.path.join(credential_dir,'raspberry-wake.json')
 
     store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, 'https://www.googleapis.com/auth/calendar')
-        flow.user_agent = 'Google Calendar API Python Quickstart'
-        credentials = tools.run(flow, store)
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=SCOPE)
+        flow.user_agent = 'Raspberry Wake'
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
+
     return credentials
+
 
 def calendar_event_query():
     credentials = get_credentials()
-    http        = credentials.authorize(httplib2.Http())
-    service     = discovery.build('calendar', 'v3', http=http)
-    today       = datetime.today()
-    events      = service.events().list(singleEvents=True, calendarId=CALENDAR_ID).execute()
-    now         = today.strftime('%Y-%m-%dT%H:%M')
-    songfile    = random.choice(os.listdir(MP3_FOLDER))
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    today = datetime.today()
+    events = service.events().list(singleEvents=True, calendarId=CALENDAR_ID).execute()
 
     for i, event in enumerate(events['items']):
         name = event['summary'].lower()
@@ -49,12 +64,12 @@ def calendar_event_query():
         repeat = True if description.lower() == 'repeat' else False
         now = today.strftime('%Y-%m-%dT%H:%M')
 
-        if start >= now:
+        if start >= now and description == 'wake':
             logger.debug('Event #%s, Name: %s, Start: %s', i, name, start)
 
             if start == now:
                 mp3_files = random.choice(os.listdir(MP3_FOLDER))
-                command = 'afplay \'{}/{}\''.format(MP3_FOLDER, mp3_files) # replace afplay with mpg123 for Raspberry or Linux
+                command = 'mpg123 \'{}/{}\''.format(MP3_FOLDER, mp3_files)
                 logger.info('Event %s starting. Playing mp3 file %s...', name, mp3_files)
                 os.system(command)
 
