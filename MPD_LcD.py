3ec6526c8c7464e8efa466e02fc27473fc8473d2
AiGreek <AiGreek@users.noreply.github.com>
2016-07-14 10:15:43 +0200

    Update MPD_LcD.py

--- MPD_LcD.py
+++ MPD_LcD.py
@@ -11,16 +11,13 @@ LCD_D5 = 17
 LCD_D6 = 21
 LCD_D7 = 22
 
-
-# Define some device constants
-LCD_WIDTH = 16  # Maximum characters per line
+LCD_WIDTH = 16
 LCD_CHR = True
 LCD_CMD = False
 
-LCD_LINE_1 = 0x80  # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0  # LCD RAM address for the 2nd line
+LCD_LINE_1 = 0x80
+LCD_LINE_2 = 0xC0
 
-# Timing constants
 E_PULSE = 0.0005
 E_DELAY = 0.0005
 
@@ -30,13 +27,13 @@ PAUSE = 16
 
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)  # Use BCM GPIO numbers
-GPIO.setup(LCD_E, GPIO.OUT)  # E
-GPIO.setup(LCD_RS, GPIO.OUT)  # RS
-GPIO.setup(LCD_D4, GPIO.OUT)  # DB4
-GPIO.setup(LCD_D5, GPIO.OUT)  # DB5
-GPIO.setup(LCD_D6, GPIO.OUT)  # DB6
-GPIO.setup(LCD_D7, GPIO.OUT)  # DB7
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LCD_E, GPIO.OUT)
+GPIO.setup(LCD_RS, GPIO.OUT)
+GPIO.setup(LCD_D4, GPIO.OUT)
+GPIO.setup(LCD_D5, GPIO.OUT)
+GPIO.setup(LCD_D6, GPIO.OUT)
+GPIO.setup(LCD_D7, GPIO.OUT)
 
 GPIO.setup(PREV, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(PAUSE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -44,21 +41,18 @@ GPIO.setup(NEXT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 def lcd_init():
-    # Initialise display
-    lcd_byte(0x33, LCD_CMD)  # 110011 Initialise
-    lcd_byte(0x32, LCD_CMD)  # 110010 Initialise
-    lcd_byte(0x06, LCD_CMD)  # 000110 Cursor move direction
-    lcd_byte(0x0C, LCD_CMD)  # 001100 Display On,Cursor Off, Blink Off
-    lcd_byte(0x28, LCD_CMD)  # 101000 Data length, number of lines, font size
-    lcd_byte(0x01, LCD_CMD)  # 000001 Clear display
+    lcd_byte(0x33, LCD_CMD)
+    lcd_byte(0x32, LCD_CMD)
+    lcd_byte(0x06, LCD_CMD)
+    lcd_byte(0x0C, LCD_CMD)
+    lcd_byte(0x28, LCD_CMD)
+    lcd_byte(0x01, LCD_CMD)
     sleep(E_DELAY)
 
 
 def lcd_byte(bits, mode):
 
-    GPIO.output(LCD_RS, mode)  # RS
-
-    # High bits
+    GPIO.output(LCD_RS, mode)
     GPIO.output(LCD_D4, False)
     GPIO.output(LCD_D5, False)
     GPIO.output(LCD_D6, False)
@@ -72,10 +66,8 @@ def lcd_byte(bits, mode):
     if bits & 0x80 == 0x80:
         GPIO.output(LCD_D7, True)
 
-    # Toggle 'Enable' pin
     lcd_toggle_enable()
 
-    # Low bits
     GPIO.output(LCD_D4, False)
     GPIO.output(LCD_D5, False)
     GPIO.output(LCD_D6, False)
@@ -89,12 +81,10 @@ def lcd_byte(bits, mode):
     if bits & 0x08 == 0x08:
         GPIO.output(LCD_D7, True)
 
-    # Toggle 'Enable' pin
     lcd_toggle_enable()
 
 
 def lcd_toggle_enable():
-    # Toggle enable
     sleep(E_DELAY)
     GPIO.output(LCD_E, True)
     sleep(E_PULSE)
@@ -103,12 +93,8 @@ def lcd_toggle_enable():
 
 
 def lcd_string(message, line):
-    # Send string to display
-
     message = message.ljust(LCD_WIDTH, " ")
-
     lcd_byte(line, LCD_CMD)
-
     for i in range(LCD_WIDTH):
         lcd_byte(ord(message[i]), LCD_CHR)
 
