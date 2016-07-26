296c8f75274ec2e8e0275df3c65a49f717d54a43
AiGreek <AiGreek@users.noreply.github.com>
2016-07-26 11:57:26 +0200

    Update Matrix_Clock.py

--- Matrix_Clock.py
+++ Matrix_Clock.py
@@ -2,44 +2,58 @@
 # -*- coding: utf-8 -*-
 
 import max7219.led as led
-import datetime
 import time
-from max7219.font import proportional, SINCLAIR_FONT, TINY_FONT, CP437_FONT
-from random import randrange
 import locale
+import Adafruit_DHT as dht
 
-device = led.matrix(cascaded=5)
+DHT22_PIN = 4
+SPI_DEVICE_NUMBER = 5
+
+device = led.matrix(cascaded=SPI_DEVICE_NUMBER)
 device.brightness(1)
 locale.setlocale(locale.LC_ALL, '')
 
-while(True):
+
+try:
+    while (True):
+        humidity, temperature = dht.read_retry(dht.DHT22, DHT22_PIN)
         clock = time.strftime("%H:%M")
+        minutes = int(time.strftime("%M"))
         seconds = int(time.strftime("%S"))
         month = time.strftime(" %m")
         day = time.strftime(" %d")
         year = time.strftime("%Y")
         for n, c in enumerate(clock):
+            device.letter(n, ord(c))
+
+        if seconds == 59:
+            device.clear()
+            device.show_message('Temp : ' + format(temperature, '.2f') + 'C')
+            device.clear()
+            device.show_message('Hum : ' + format(humidity, '.2f') + '%')
+            device.clear()
+            for row in range(8):
+                device.scroll_up()
+                time.sleep(0.1)
+            for n, c in enumerate(day):
                 device.letter(n, ord(c))
-        if seconds == 59 :
-                for row in range(8):
-                        device.scroll_up()
-                        time.sleep(0.1)
-                for n, c in enumerate(day):
-                        device.letter(n, ord(c))
-                time.sleep(1)
-                for row in range(8):
-                        device.scroll_down()
-                        time.sleep(0.1)
-                for n, c in enumerate(month):
-                        device.letter(n, ord(c))
-                time.sleep(1)
-                for row in range(8):
-                        device.scroll_down()
-                        time.sleep(0.1)
-                for n, c in enumerate(year):
-                        device.letter(n, ord(c))
-                time.sleep(1)
-                for row in range(8):
-                        device.scroll_down()
-                        time.sleep(0.1)
-                device.show_message(time.strftime("%d %B %Y"))
+            time.sleep(1)
+            for row in range(8):
+                device.scroll_down()
+                time.sleep(0.1)
+            for n, c in enumerate(month):
+                device.letter(n, ord(c))
+            time.sleep(1)
+            for row in range(8):
+                device.scroll_down()
+                time.sleep(0.1)
+            for n, c in enumerate(year):
+                device.letter(n, ord(c))
+            time.sleep(1)
+            for row in range(8):
+                device.scroll_down()
+                time.sleep(0.1)
+            device.show_message(time.strftime("%d %B %Y"))
+
+except KeyboardInterrupt:
+    device.clear()
